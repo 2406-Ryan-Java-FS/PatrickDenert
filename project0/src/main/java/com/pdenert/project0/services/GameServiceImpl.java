b527139dbcfd3cf5a6923d999ed95cbc4cdd45e3
patrickdenert9 <patrickdenert@gmail.com>
2024-07-01 18:05:33 -0400

    add create findById and update methods in controller

--- project0/src/main/java/com/pdenert/project0/services/GameServiceImpl.java
+++ project0/src/main/java/com/pdenert/project0/services/GameServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameServiceImpl implements GameService {
@@ -14,7 +15,24 @@ public class GameServiceImpl implements GameService {
     GameRepo gr;
 
     @Override
-    public List<Game> getAllGames() {
+    public List<Game> getAllGames() {                           // get all games from db
         return (List<Game>) gr.findAll();
+    }
+
+    @Override
+    public Game getGameById(int id) {                           // get game from id
+        Optional<Game> game = gr.findById(id);
+        //return Optional.of(game.orElseGet(Game::new));
+        return game.orElseGet(Game::new);                       // unwrap optional
+    }
+
+    @Override
+    public Game createGame(Game game) {                         //save new game in db
+        return gr.save(game);
+    }
+
+    @Override
+    public Game updateGame(Game game) {                         // update game in db
+        return gr.save(game);
     }
 }
