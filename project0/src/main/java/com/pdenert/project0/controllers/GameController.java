df9f2dec6d9dcd77fc26ac221e056159bb40ceb9
Patrick Denert <pdenert@gmail.com>
2024-07-02 00:39:02 -0400

    update all controller methods to return ResponseEntities

--- project0/src/main/java/com/pdenert/project0/controllers/GameController.java
+++ project0/src/main/java/com/pdenert/project0/controllers/GameController.java
@@ -3,6 +3,7 @@ package com.pdenert.project0.controllers;
 import com.pdenert.project0.models.Game;
 import com.pdenert.project0.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,27 +20,27 @@ public class GameController {
     }
 
     @GetMapping("/games")
-    public List<Game> getAllGames() {                       //return list of all games in db
-        return gs.getAllGames();
+    public ResponseEntity<List<Game>> getAllGames() {                       //return list of all games in db
+        return ResponseEntity.status(200).body(gs.getAllGames());
     }
 
     @GetMapping("/games/{id}")
-    public Game getGameById(@PathVariable int id){          //return game with correct id
+    public ResponseEntity<Game> getGameById(@PathVariable int id){          //return game with correct id
         // game = gs.getGameById(id);
         //return game.orElseGet(Game::new);
-        return gs.getGameById(id);
+        return ResponseEntity.status(200).body(gs.getGameById(id));
     }
 
     @PostMapping("/games")
-    public Game createGame(@RequestBody Game game){         //create new game from requestbody
-        if (game.getName() == null) {                       //check if name is blank
+    public ResponseEntity<Game> createGame(@RequestBody Game game){         //create new game from requestbody
+        if (game.getName() == null) {                                       //check if name is blank
             return null;
         }
-        return gs.createGame(game);                         //save game in db if pass checks
+        return  ResponseEntity.status(200).body(gs.createGame(game));       //save game in db if pass checks
     }
 
     @PatchMapping("/games/{id}")
-    public Game updateGame(@RequestBody Game game, @PathVariable int id){       //update game rating
+    public ResponseEntity<Game> updateGame(@RequestBody Game game, @PathVariable int id){       //update game rating
         Game updatedGame = gs.getGameById(id);                                  //get existing game from db
 
         if(updatedGame == null){                                                //check if game exists
@@ -48,12 +49,12 @@ public class GameController {
 
         updatedGame.setRating(game.getRating());                                //update game rating
 
-        return gs.updateGame(updatedGame);                                      //save updates into db
+        return ResponseEntity.status(200).body(gs.updateGame(updatedGame));     //save updates into db
     }
 
     @DeleteMapping("/games/{id}")
-    public int deleteGame(@PathVariable int id){                                //delete game from db
+    public ResponseEntity<Integer> deleteGame(@PathVariable int id){            //delete game from db
         gs.deleteGame(id);
-        return id;                                                              //return id if deleted
+        return ResponseEntity.status(200).body(id);                             //return id if deleted
     }
 }
--- project0/src/main/java/com/pdenert/project0/models/User.java
+++ project0/src/main/java/com/pdenert/project0/models/User.java
@@ -1,5 +1,15 @@
 package com.pdenert.project0.models;
 
-public class User {
+import jakarta.persistence.*;
 
+@Entity
+@Table(name="users")
+public class User {
+    @Column(name="user_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer user_id;
+
+    @Column(name="password")
+    private String password;
 }
